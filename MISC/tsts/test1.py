e053ae33399aaf1b387d08e79e1eddf884031fd0
Adedoyin Oketola <oketola@LabImage2016-17>
2016-11-10 15:14:19 -0800

    fix errors

--- MISC/tsts/test1.py
+++ MISC/tsts/test1.py
@@ -1,15 +1,17 @@
 # Given a number, e.g 623315, what is the largest number that can be gotten when
 # you take average of any 2 adjacent numbers?
+# [:i] means give me every letter before i
+# [i:] means give me every letter from i to the end
+from __future__ import division
 import math
 def solution(number):
+    number = str(number)
     maxSoFar = 0
     for i in range(0,len(number)-1):
-        newDigit = math.ceil((int(number[i]) + int(number[i+1]))/2)
-        newDigit = int(newDigit)
-        newNumber = number[:i] + str(newDigit) + number[i+2:]
-        if maxSoFar < int(newNumber):
-            maxSoFar = int(newNumber)
-    print(maxSoFar)
+        newDigit = int(math.ceil((int(number[i]) + int(number[i+1]))/2))
+        newNumber = int(number[:i] + str(newDigit) + number[i+2:])
+        if maxSoFar < newNumber:
+            maxSoFar = newNumber
     return maxSoFar
 
 
@@ -32,25 +34,32 @@ def solution(number):
 
 
 def main():
-    max = solution("623315")
-    print(max)
+    maxNum = solution(623315)
+    print("maximum number from average is", maxNum)
+    path_str = "dir1 dir11 dir12
+  picture.jpeg
+  dir121
+  file1.txt
+dir2
+ file2.gif"
+    longest_path = path_solution(path_str)
 
-str = "dir1\n" \
-      " dir11\n" \
-      " dir12\n" \
-      "     pic.jpeg\n" \
-      "     dir121\n" \
-      "dir2\n" \
-      " file2"
+# str = "dir1\n" \
+#       " dir11\n" \
+#       " dir12\n" \
+#       "     pic.jpeg\n" \
+#       "     dir121\n" \
+#       "dir2\n" \
+#       " file2"
 main()
 
 # find the path to the picture file and print it out. shortest or longest
 #
-# dir1
-#  dir11
-#  dir12
-#   picture.jpeg
-#   dir121
-#   file1.txt
-# dir2
-#  file2.gif
+dir1
+ dir11
+ dir12
+  picture.jpeg
+  dir121
+  file1.txt
+dir2
+ file2.gif
